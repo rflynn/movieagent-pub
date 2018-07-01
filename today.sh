cf8a474ac602dbeef5ff59baffe8a17e3e21c672
Ryan Flynn <parseerror+github@gmail.com>
2018-07-01 08:49:43 -0400

    today.sh

---
+++ today.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+. $HOME/.bashrc
+
+PGPASSWORD=password psql -h localhost -U postgres -d postgres -c 'select * from rf_whatsplaying' > today.txt
+git add today.txt
+
+PGPASSWORD=password psql -h localhost -U postgres -d postgres -c 'select * from rf_whatsplaying_fd' > today_fd.txt
+git add today_fd.txt
+
+PGPASSWORD=password psql -h localhost -U postgres -d postgres -c 'select * from rf_whatsplaying_fd_gantt' > today_fd_gantt.txt
+git add today_fd_gantt.txt
+
+(cd $HOME/src/movieagent/data/seen && ../../venv/bin/python html.py > $HOME/src/movieagent-pub/whatsplaying.html)
+git add whatsplaying.html
+
+git commit -m today
+git push -u origin master
